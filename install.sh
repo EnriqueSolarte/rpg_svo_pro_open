e0b8da544f413e59527da3e8dc97b4114be4980a
EnriqueSolarte <enrique.solarte.pardo@gmail.com>
2021-08-17 14:02:45 +0800

    WIP: install.sh added

--- install.sh
+++ install.sh
@@ -10,3 +10,21 @@ sudo apt-get install libglew-dev libopencv-dev libyaml-cpp-dev
 sudo apt-get install libblas-dev liblapack-dev libsuitesparse-dev
 
 ROS-DISTRO=melodic
+
+cd $ROS_WORKSPACES
+mkdir $ROS_WORKSPACES/svo_ws && cd svo_ws
+
+# see below for the reason for specifying the eigen path
+catkin config --init --mkdirs --extend /opt/ros/$ROS-DISTRO --cmake-args -DCMAKE_BUILD_TYPE=Release -DEIGEN3_INCLUDE_DIR=/usr/include/eigen3
+cd src
+
+# git clone git@github.com:uzh-rpg/rpg_svo_pro_open.git
+# ! VSLAB VERSION
+git clone https://github.com/EnriqueSolarte/rpg_svo_pro_open.git
+git checkout vslab
+
+vcs-import < ./rpg_svo_pro_open/dependencies.yaml
+touch minkindr/minkindr_python/CATKIN_IGNORE
+# vocabulary for place recognition
+cd rpg_svo_pro_open/svo_online_loopclosing/vocabularies && ./download_voc.sh
+cd ../../..
